5792a9a9277f9edc2d85b77e6bd1b29c2bff7ed7
Nikita Domnitskii <nikita@domnitskii.me>
2022-04-20 16:22:50 +0300

    routing

--- src/krevedkokun/clj_http_comp.clj
+++ src/krevedkokun/clj_http_comp.clj
@@ -1,61 +1,99 @@
 (ns krevedkokun.clj-http-comp
-  (:require [org.httpkit.server :as httpkit]
+  (:require [charred.api :as charred]
+            [cheshire.core :as cheshire]
+            [clojure.pprint :as pp]
             [com.appsflyer.donkey.core :as donkey]
-            [org.httpkit.client :as http]
             [immutant.web :as immutant]
-            [ring.adapter.jetty])
+            [jsonista.core :as jsonista]
+            [org.httpkit.client :as http]
+            [org.httpkit.server :as httpkit]
+            [pohjavirta.server :as pohjavirta]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn handler
+  [{:keys [body uri] :as req}]
+  #_(pp/pprint req)
+  (case uri
+    "/_perf1"
+    {:status 200
+     :body (or body "{\"message\":\"Hello\"}")}
 
-(defn handler [req]
-  {:status 200
-   :body (or (:body req) "{\"message\":\"Hello\"}")})
+    "/cheshire"
+    {:status 200
+     :body (some-> body
+                   (slurp)
+                   (cheshire/parse-string)
+                   (cheshire/generate-string))}
+
+    "/jsonista"
+    {:status 200
+     :body (some-> body
+                   (slurp)
+                   (jsonista/read-value)
+                   (jsonista/write-value-as-string))}
+
+    "/charred"
+    {:status 200
+     :body (some-> body
+                   (slurp)
+                   (charred/read-json)
+                   (charred/write-json-str))}
+
+    {:status 404
+     :body "{\"message\": \"no such route\"}"}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  [& _args])
 
 (comment
 
-(def stop-httpkit
-  (httpkit/run-server handler {:port 5001
-                               :thread 100}))
+  (some-> "{\"message\":\"Hello\"}"
+          (cheshire/parse-string)
+          (cheshire/generate-string))
 
-(stop-httpkit)
+  (def stop-httpkit
+    (httpkit/run-server #'handler {:port 5001
+                                   :thread 100}))
 
-
-(def stop-jetty
-  (ring.adapter.jetty/run-jetty handler {:port 5002
-                                         :max-threads 100
-                                         :join? false}))
-
-(.stop stop-jetty)
+  (stop-httpkit)
 
 
-(def stop-immutant
-  (immutant/run handler {:host "0.0.0.0" :port 5003 :path "/"}))
+  (def stop-jetty
+    (jetty/run-jetty #'handler {:port 5002
+                                :max-threads 100
+                                :join? false}))
 
-(.stop (immutant/server {:host "0.0.0.0" :port 5003}))
-
-(immutant/stop {:host "0.0.0.0" :port 5003})
-(.stop )
-
-@(http/get "http://localhost:5003/_perf1")
-
-@(http/get "http://localhost:5003")
-
-(slurp (:body @(http/get "http://localhost:5001")))
-
-@(http/get "http://localhost:5002")
+  (.stop stop-jetty)
 
 
+  (def stop-immutant
+    (immutant/run #'handler {:host "0.0.0.0" :port 5003 :path "/"}))
+
+  (.stop (immutant/server {:host "0.0.0.0" :port 5003}))
+
+  (immutant/stop {:host "0.0.0.0" :port 5003})
+
+  (def pohjavirta
+    (pohjavirta/create #'handler {:port 5004
+                                  :worker-threads 100
+                                  :host "0.0.0.0"}))
+
+  (pohjavirta/start pohjavirta)
+
+  (pohjavirta/stop pohjavirta)
+
+  @(http/get "http://localhost:5003/_perf1")
+
+  @(http/get "http://localhost:5003")
+
+  (slurp (:body @(http/get "http://localhost:5001")))
+
+  @(http/get "http://localhost:5002")
 
 
 
 
-)
+
+
+  )
